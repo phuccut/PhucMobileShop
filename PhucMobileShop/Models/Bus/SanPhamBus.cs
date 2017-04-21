8efc9e4853ad035b3933e81f88b6ce3082fe092b
phuccut <phuccut@gmail.com>
2017-04-21 11:34:22 +0700

    Danh sách và chi tiết sản phẩm

---
+++ PhucMobileShop/Models/Bus/SanPhamBus.cs
@@ -0,0 +1,23 @@
+﻿using PhucMobileConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhucMobileShop.Models.Bus
+{
+    public class SanPhamBus
+    {
+        public static IEnumerable<sanpham> DanhSach()
+        {
+            var db = new PhucMobileConnectionDB();
+            return db.Query<sanpham>("SELECT * FROM sanpham");
+        }
+        public static sanpham ChiTiet(int id)
+        {
+            var db = new PhucMobileConnectionDB();
+            return db.SingleOrDefault<sanpham>("SELECT * FROM sanpham WHERE MaSanPham = @0", id);
+            
+        }
+    }
+}
